6fbe414f8451a650d1d696bc4119213d01f8ee07
Al Zee <z@alz.ee>
2024-01-11 15:14:15 +0800

    nth

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -27,14 +27,14 @@ class ProductsController extends AbstractController
         $locale = $request->getLocale();
         $request = $this->data->findNodeByRegionAndLocale('request', $locale);
         
-        $hero = $this->data->findNodeByCategoryAndRegion($category, 'products-hero', 1)[0];
+        $hero = $this->data->findNodeByCategoryAndRegion($category, 'products-hero', 1);
         $sliders1 = $this->data->findNodeByCategoryAndTag($category, 'products-slider-1', 3);
         $sliders2 = $this->data->findNodeByCategoryAndTag($category, 'products-slider-2', 3);
         $sliders3 = $this->data->findNodeByCategoryAndTag($category, 'products-slider-3', 3);
         $sliders4 = $this->data->findNodeByCategoryAndTag($category, 'products-slider-4', 3);
         $sliders5 = $this->data->findNodeByCategoryAndTag($category, 'products-slider-5', 3);
         $sliders6 = $this->data->findNodeByCategory('gaizhuangche-1', 3);
-        $spec1 = $this->data->findNodeByCategoryAndTag($category, 'products-spec-1', 1)[0];
+        $spec1 = $this->data->findNodeByCategoryAndTag($category, 'products-spec-1', 1);
         $improves = $this->data->findNodeByCategoryAndTag($category, 'products-improves', 5);
         
         $info = $this->data->getInfo($locale);
@@ -43,7 +43,7 @@ class ProductsController extends AbstractController
           'class' => 'page-products position-absolute',
           'page_title' => $this->translator->trans('Products'),
           'request' => $request,
-          'hero' => $hero,
+          'hero' => empty($hero)? [] : $hero[0],
           'improves' => $improves,
           'sliders1' => $sliders1,
           'sliders2' => $sliders2,
@@ -51,7 +51,7 @@ class ProductsController extends AbstractController
           'sliders4' => $sliders4,
           'sliders5' => $sliders5,
           'sliders6' => $sliders6,
-          'spec1' => $spec1,
+          'spec1' => empty($spec1)? [] : $spec1[0],
           'info' => $info,
         ];
         return $this->render('products/index.html.twig', $data);
