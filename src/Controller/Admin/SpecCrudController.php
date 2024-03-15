4924e56e08076690b231c6c8f289047145c37680
Al Zee <z@alz.ee>
2024-03-15 09:52:16 +0800

    TextareFeild for spec value

--- src/Controller/Admin/SpecCrudController.php
+++ src/Controller/Admin/SpecCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class SpecCrudController extends AbstractCrudController
 {
@@ -18,6 +19,6 @@ class SpecCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield TextField::new('name', 'Spec Name');
-        yield TextField::new('value', 'Spec Value');
+        yield TextareaField::new('value', 'Spec Value')->setNumOfRows(8);
     }
 }
